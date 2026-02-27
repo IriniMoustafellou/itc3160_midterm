f34a1d2882bd152d5fc3e577aded9a449681557f
GMichos-bit <g.michos@acg.edu>
2026-02-27 04:50:15 +0200

    Update question4.sql
    Added mc_artist and similar where necessary. Restructured the code to a cleaner view. Added order by. Changed the last inner join

--- question4.sql
+++ question4.sql
@@ -1,7 +1,12 @@
---Q4 !
-select full_name, title
+--Q4
+select mc_artist.full_name, mc_artwork.title
 from mc_artist
-inner join mc_artwork on mc_artist.artist_id = mc_artwork.artist_id
-inner join mc_artwork_exh on mc_artwork.artwork_id = mc_artwork_exh.artwork_id
---inner join mc_artwork_exh on mc_artwork_exh.exhibition_id = MC_EXHIBITION.EXHIBITION_ID
---where title = 'Modern Voices';
+inner join mc_artwork 
+  on mc_artist.artist_id = mc_artwork.artist_id
+inner join mc_artwork_exh 
+  on mc_artwork.artwork_id = mc_artwork_exh.artwork_id
+inner join mc_exhibition 
+  on  mc_exhibition.exhibition_id = mc_artwork_exh.exhibition_id 
+where mc_exhibiton.title = 'Modern Voices'
+order by mc_artist.full_name;
+
