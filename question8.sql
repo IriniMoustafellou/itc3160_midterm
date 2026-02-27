56f1086165d63bebe89e451b84616f8915569d3f
GMichos-bit <g.michos@acg.edu>
2026-02-27 05:28:42 +0200

    Update question8.sql
    Added mc_artwork and like where necessary. Restructured the code for cleanliness. Fixed a possible logic error in the and or statement. Fixed an error caused by left join removing all null rows

--- question8.sql
+++ question8.sql
@@ -1,8 +1,13 @@
 --Q8
 select mc_artwork.title, mc_artwork_exh.label_text
 from mc_artwork
-left join mc_artwork_exh on mc_artwork.artwork_id = mc_artwork_exh.artwork_id
-inner join mc_exhibition on mc_artwork_exh.exhibition_id = mc_exhibition.exhibition_id
-inner join mc_curator on mc_exhibition.curator_id = mc_curator.curator_id
-where label_text is not null and mc_curator.full_name like '%Maria'or mc_curator.full_name like '%Pappas'
-order by title;
+inner join mc_artwork_exh 
+  on mc_artwork.artwork_id = mc_artwork_exh.artwork_id
+inner join mc_exhibition 
+  on mc_artwork_exh.exhibition_id = mc_exhibition.exhibition_id
+inner join mc_curator 
+  on mc_exhibition.curator_id = mc_curator.curator_id
+where mc_artwork_exh.label_text is not null 
+  and (mc_curator.full_name like '%Maria'
+  or mc_curator.full_name like '%Pappas')
+order by mc_artwork.title;
