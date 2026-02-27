daebc592e3f0afed3dddf79be05803cf7da77199
GMichos-bit <g.michos@acg.edu>
2026-02-27 05:46:33 +0200

    Update question9.sql
    Restructured the code for clarity

--- question9.sql
+++ question9.sql
@@ -2,5 +2,7 @@
 create view view_exhibition as
 select mc_exhibition.exhibition_id, mc_exhibition.title as exhibition_title, mc_artwork.artwork_id, mc_artwork.title as artwork_title
 from mc_exhibition
-inner join mc_artwork_exh on mc_exhibition.exhibition_id = mc_artwork_exh.exhibition_id
-inner join mc_artwork on mc_artwork_exh.artwork_id = mc_artwork.artwork_id;
+inner join mc_artwork_exh 
+  on mc_exhibition.exhibition_id = mc_artwork_exh.exhibition_id
+inner join mc_artwork
+  on mc_artwork_exh.artwork_id = mc_artwork.artwork_id;
