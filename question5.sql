6e4ea9b786254bf7b7acc2332850c9df9a2bbf01
GMichos-bit <g.michos@acg.edu>
2026-02-27 04:21:15 +0200

    Update question5.sql
    Cleared up structure

--- question5.sql
+++ question5.sql
@@ -1,5 +1,8 @@
 --Q5
 select mc_artwork.title
 from mc_artwork
-inner join mc_collection on mc_collection.collection_id = mc_artwork.collection_id
-where est_value >(select avg(est_value) from mc_artwork);
+inner join mc_collection 
+  on mc_collection.collection_id = mc_artwork.collection_id
+where mc_artwork.est_value > 
+  (select avg(mc_artwork.est_value) from mc_artwork);
+
